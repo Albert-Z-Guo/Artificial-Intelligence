e1c91a80f3e9369e0fb9c76426d99d2dd5991f6e
A. Z. Guo <albertguotrs@hotmail.com>
2018-12-11 10:57:09 -0600

    comments cleaning

--- A* search algorithm/code.py
+++ A* search algorithm/code.py
@@ -3,8 +3,6 @@ import heapq as minheap
 
 def a_star_search (dis_map, time_map, start, end):
 	path = []
-	# TODO Put your code here.
-	# be sure to call the imported function expand to get the next list of nodes
 	open_nodes = []
 	closed_nodes = []
 	node_expansion_dict = {}
